1538e94840bf43a956f411fc74f003f5272d1db9
$6N3100-KNDNR7IOKFM9 <freund.lf@gmail.com>
2022-01-23 23:28:58 +0100

    With Unversionned files

---
+++ app/src/main/java/com/example/hello22kot/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.hello22kot
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        println("Youhou")
+    }
+}
